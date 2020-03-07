f24c33a6e120b871ec401477f6360cfabcd05cf9
Devillez Louis <louis.devillez@student.ecp.fr>
2020-03-07 12:49:46 +0100

    add_low_level

--- V1_pi/motor_card.hpp
+++ V1_pi/motor_card.hpp
@@ -1,15 +1,16 @@
 using namespace std;
 #include <string>
 
-
+double limit(double val, double limit);
 
 class motor_card{
     public:
     motor_card(string id, int type);
     ~motor_card();
-    void set_voltage(int volt);
+    void init();
+    void set_voltage(double volt);
     void ctrl_motor(int state);
-    void set_speed(int speed);
+    void set_speed(double speed);
 
 
     private:
@@ -18,4 +19,18 @@ class motor_card{
     // 1 - UCL1 for the first motor
     // 2 - UCL2 for the second motor
     int type;
+
+    // For UCL card
+    // low-level controller
+    double wheel_speed;
+    double error_integ;
+    double saturation;
+    double alpha;
+    double kp;
+    double ki;
+    double gearbox;
+    double khpi;
+    double deltaT;
+    double limitV;
+    double limitI;
 };
