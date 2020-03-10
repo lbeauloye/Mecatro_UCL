23ca23afee5472e5e14f52d29ddd7d81821f2375
Devillez Louis <louis.devillez@student.ecp.fr>
2020-03-10 17:15:04 +0100

    old main

--- V1_pi/main.cpp
+++ V1_pi/main.cpp
@@ -5,74 +5,17 @@
 #include <unistd.h>
 #include "can.hpp"
 #include "mid_level.hpp"
-#include <signal.h>
-#include <iostream>
-#include <fstream>
 
 // Garbage collection
-
-void signal_callback_handler(int signum) {
-   send_message("708", "1EFFFF");
-    // Terminate program
-   exit(signum);
-}
 
 int main(int argc, char *argv[])
 {
 
     motor_card **motors =(motor_card**) calloc(4,sizeof(motor_card));
-    motors[0] = new motor_card("708",1);
-    motors[0]->init();
-    motors[0]->ctrl_motor(1);
-    //motors[0]->set_voltage(-100);
-    signal(SIGINT, signal_callback_handler);
-
-
-    double a, b, c, d, kp = 0.06337, ki = 0.622;
-    std::fstream myfile("../speed_robot.txt", std::ios_base::in);
-    myfile >> a >> b >> c >> d;
-    myfile.close();
-    int cnt = 0;
-    double speed = 0;
-    int n = 60;
-
-
-    while(true){
-        
-        usleep(10000);
-        myfile.open("../speed_robot.txt", std::ios_base::in);
-        myfile >> a >> b >> c >> d;
-        myfile.close();
-
-        myfile.open("../pid.txt", std::ios_base::in);
-        myfile >> kp >> ki;
-        myfile.close();
-
-        motors[0]->set_kp(kp);
-        motors[0]->set_ki(ki);
-
-        //printf("%f %f\n", kp, ki);
-
-        motors[0]->set_old_speed(d);
-        cnt ++;
-
-        if(cnt == n){
-            speed = 20;
-        } else if(cnt == 2*n){
-            speed = -20;
-        } else if(cnt == 3*n){
-            speed = 0;
-            cnt = 0;
-        }
-        
-        motors[0]->set_speed(speed);
-        //motors[0]->set_voltage(100);
-        system("rm -rf ../data_robot.txt");
-        std::fstream mydata("../data_robot.txt", std::ios_base::out);
-        mydata.seekp(0);
-        mydata << speed << " " << d;
-        mydata.close();
-    }
-
-
+    motors[0] = new motor_card("408",1);
+    motors[1] = new motor_card("408",2);
+    motors[2] = new motor_card("408",1);
+    motors[3] = new motor_card("708",2);
+    mid_level_ctrl *ctrl = new mid_level_ctrl(10,10,3,0,motors);
+    ctrl->set_wheel_speed(5,0,0,0);
 }
